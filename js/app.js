e1db9078055adb23c20d5f2663cc6aae70258c98
marifercam <fm9705484@gmail.com>
2025-10-15 12:28:40 -0600

    02 Add guitars in cart

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import { db } from './guitarras.js';
 
 const divContainer = document.querySelector('main div');
+const carrito = []
 
 const createDiv = (guitar) => {
     const div = document.createElement('div');
@@ -13,6 +14,7 @@ const createDiv = (guitar) => {
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$${ guitar.precio}</p>
                     <button 
+                    data-id="${ guitar.id }"
                         type="button"
                         class="btn btn-dark w-100 "
                     >Agregar al Carrito</button>
@@ -25,3 +27,24 @@ const createDiv = (guitar) => {
 db.forEach( guitar => {
     divContainer.appendChild( createDiv(guitar) );
 })
+const cardClicked = (e) =>{
+    if(e.target.classList.contains('btn')){
+        //console.log('Le diste al botón',e.target.getAttribute('data-id'))
+        const idGuitar = Number(e.target.getAttribute('data-id'))
+       // const indexdb = db.findIndex(guitar => guitar.id === Number(idGuitar))
+       //si la guitarra ya existe incrementamos cantidad 1
+       //En caso de que no agregamos una nueva
+    if(!carrito.some(g => g.id === idGuitar)){
+        carrito.push({
+            ...db[idGuitar],
+            cantidad: 1
+    })
+        
+    } else{
+        const idCarrito = carrito.findIndex(g => g.id === idGuitar)
+        const currentGuitar = carrito[idCarrito]
+        currentGuitar.cantidad++
+    }
+    console.log(carrito)
+}
+divContainer.addEventListener('click', cardClicked)
