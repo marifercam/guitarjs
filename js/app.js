580535e8032adf763b5e3e5f5e8b7da49c199c79
unknown <fm9705484@gmail.com>
2025-10-22 10:00:20 -0600

    Las guitarras

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 import { db } from './guitarras.js';
 
 const divContainer = document.querySelector('main div');
-const carrito = []
+const carritoContainer = document.querySelector('#carrito');
+let carrito =[]
 
 const createDiv = (guitar) => {
     const div = document.createElement('div');
@@ -12,9 +13,9 @@ const createDiv = (guitar) => {
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">${ guitar.nombre }</h3>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$${ guitar.precio}</p>
+                    <p class="fw-black text-primary fs-3">${ guitar.precio}</p>
                     <button 
-                    data-id="${ guitar.id }"
+                        data-id="${guitar.id}"
                         type="button"
                         class="btn btn-dark w-100 "
                     >Agregar al Carrito</button>
@@ -25,26 +26,109 @@ const createDiv = (guitar) => {
 
 //utilizando interadores
 db.forEach( guitar => {
-    divContainer.appendChild( createDiv(guitar) );
+divContainer.appendChild( createDiv(guitar) );
 })
-const cardClicked = (e) =>{
+
+const carritoCliked = (e) => {
     if(e.target.classList.contains('btn')){
-        //console.log('Le diste al botón',e.target.getAttribute('data-id'))
-        const idGuitar = Number(e.target.getAttribute('data-id'))
-       // const indexdb = db.findIndex(guitar => guitar.id === Number(idGuitar))
-       //si la guitarra ya existe incrementamos cantidad 1
-       //En caso de que no agregamos una nueva
-    if(!carrito.some(g => g.id === idGuitar)){
-        carrito.push({
-            ...db[idGuitar],
-            cantidad: 1
-    })
-        
-    } else{
-        const idCarrito = carrito.findIndex(g => g.id === idGuitar)
-        const currentGuitar = carrito[idCarrito]
-        currentGuitar.cantidad++
+        const btn = e.target.innerText
+        const idCarrito = e.target
+        .parentElement
+        .parentElement.getAttribute('data-id')
+    const idxCarrito = carrito
+        .findIndex(g => g.id === Number (idCarrito))
+    if(btn === '-'){
+        carrito[idxCarrito].cantidad--
+    } else if (btn === '+'){
+        carrito[idxCarrito].cantidad++
+    } else if (btn === 'X'){
+        carrito = carrito.filter( g => g.id !== Number(idCarrito))
+    }else if (btn === 'VACIAR CARRITO'){
+        carrito = []
     }
-    console.log(carrito)
+    createCart(carrito)
+    }
 }
+const createCart = (carrito) =>{
+    const p = '<p class="text-center">El carrito esta vacio</p>' 
+    let total = 0
+let html = `<table class="w-100 table">
+                            <thead>
+                                <tr>
+                                    <th>Imagen</th>
+                                    <th>Nombre</th>
+                                    <th>Precio</th>
+                                    <th>Cantidad</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>`
+    carrito.forEach(g=>{
+        total += g.precio * g.cantidad
+        html+= `<tr data-id="${g.id}">
+                    <td>
+                        <img class="img-fluid" src="./img/${g.imagen}.jpg" alt="imagen guitarra">
+                    </td>
+                    <td>${ g.nombre}</td>
+                    <td class="fw-bold">
+                            $${ g.precio}
+                    </td>
+                    <td class="flex align-items-start gap-4">
+                        <button
+                            type="button"
+                            class="btn btn-dark"
+                        >-</button>
+                            ${g.cantidad}
+                        <button
+                            type="button"
+                            class="btn btn-dark"
+                        >+</button>
+                    </td>
+                    <td>
+                        <button
+                            class="btn btn-danger"
+                            type="button"
+                        >
+                            X
+                        </button>
+                    </td>
+                </tr>`
+    })
+
+    html += `</tbody>
+                    </table>
+
+                    <p class="text-end">Total pagar: <span class="fw-bold">$${ total }</span></p>
+                    <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>`;
+    if(carrito.length === 0 ){
+        carritoContainer.innerHTML = p;
+    } else {
+        carritoContainer.innerHTML = html
+    }
+}
+
+const cardClicked = (e) => {
+    if(e.target.classList.contains('btn')){
+        //console.log('le diste al botón', e.target.getAttribute('data-id'));
+        const idGuitar = e.target.getAttribute('data-id');
+        //const indexdb = db.findIndex( guitar => guitar.id === Number(idGuitar));
+        //si la guitarra ya existe en el carrito, solo aumentamos la cantidad en 1
+        //en caso contrario, la agregamos al carrito con cantidad 1
+        const idxGuitars = carrito.findIndex(g=> g.id === Number(idGuitar));
+        if( idxGuitars === -1){
+            carrito.push({
+            ...db[idGuitar - 1],
+            cantidad: 1
+        })
+        } else {
+            carrito[idxGuitars].cantidad++
+        }
+        createCart(carrito);
+    }
+}
+
+createCart(carrito)
+
+
 divContainer.addEventListener('click', cardClicked)
+carritoContainer.addEventListener('click', carritoCliked)
